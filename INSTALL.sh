bd8b486572fc61b4561c8873a9355d0800b7cc6a
Ezee1015 <routerciscoanuitti@gmail.com>
2021-12-08 11:08:24 -0300

    Soluciona un problema con NERDTree al inicio

--- INSTALL.sh
+++ INSTALL.sh
@@ -6,7 +6,11 @@ function INSTALAR {
      cp -rf Tema/colors/* ~/.vim/colors
      cp Tema/vim_forever.sh ~/.vim_forever.sh
      cp Documentacion/VIM\ -\ Documentacion.pdf ~/
+     mkdir ~/.vim
      mkdir ~/.vim/undodir
+     mkdir ~/.vim/after
+     mkdir ~/.vim/after/plugin
+     echo "autocmd VimEnter * if argc() == 0 && !exists('s:std_in') | silent NERDTree | endif" > ~/.vim/after/plugin/NERDTreeAutoExecute.vim
      echo ""
      sudo mkdir /usr/share/fonts/truetype/Ubuntu__Mono_Nerd_Font
      sudo cp Tema/ttf/*.ttf /usr/share/fonts/truetype/Ubuntu__Mono_Nerd_Font/
