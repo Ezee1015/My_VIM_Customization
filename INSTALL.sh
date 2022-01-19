dfed135625ad6b155b3b7ba11d25d8df72660914
Ezee1015 <routerciscoanuitti@gmail.com>
2022-01-19 11:01:14 -0300

    Agrega Dependencias

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,10 +2,10 @@
 
 function INSTALAR {
 
-    sudo apt install vim curl vim-gtk clangd-12 openjdk-17* clangd curl -y
-    sudo apt install vim curl vim-gtk clangd-11 openjdk-17* clangd curl -y
+    sudo apt install vim curl vim-gtk clangd-12 openjdk-17* clangd xdotool curl -y
+    sudo apt install vim curl vim-gtk clangd-11 openjdk-17* clangd xdotool curl -y
         # sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-12 100
-    sudo pacman -S neovim xclip xorg-xclipboard gvim clang
+    sudo pacman -S neovim xclip xorg-xclipboard gvim clang xdotool
     curl -sL install-node.vercel.app/lts | sudo bash
     cp Tema/vimrc ~/.vimrc
     cp -rf Tema/colors/* ~/.vim/colors/
