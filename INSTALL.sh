3e039491cc9669d2306ea6901111130d8d0e6d95
Ezee1015 <routerciscoanuitti@gmail.com>
2022-06-18 12:10:22 -0300

    Agrega una opcion mas de instalacion

--- INSTALL.sh
+++ INSTALL.sh
@@ -9,6 +9,7 @@ function INSTALAR {
         # sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-12 100
     sudo pacman -S neovim xclip xorg-xclipboard gvim clang xdotool python3
     pip install -U neovim
+    pip3 install -U neovim
         # ADICIONAL PARA EDICION EN MARKDOWN
         #sudo pacman -S pandoc markdown texlive-core
     curl -sL install-node.vercel.app/lts | sudo bash
