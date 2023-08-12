e8288c5a352e7027f90354c569ac67f43e85f505
Ezee1015 <82286822+Ezee1015@users.noreply.github.com>
2023-08-12 09:59:59 -0300

    Agrega c++ a Tree Sitter

--- Tema/init/lua/p-conf/treesitter.lua
+++ Tema/init/lua/p-conf/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "c", "lua", "java", "python", "bash", "html", "css", "php", "sql" },
+  ensure_installed = { "c", "cpp", "lua", "java", "python", "bash", "html", "css", "javascript", "sql" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   -- sync_install = false,
