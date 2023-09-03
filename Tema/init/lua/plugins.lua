7eafae2e7e83e70bfc3ccbf3a18141d3e012a87c
Ezee1015 <82286822+Ezee1015@users.noreply.github.com>
2023-09-03 15:30:12 -0300

    Reemplaza a Packer por Lazy

--- Tema/init/lua/plugins.lua
+++ Tema/init/lua/plugins.lua
@@ -1,104 +1,175 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+			"git",
+			"clone",
+			"--filter=blob:none",
+			"https://github.com/folke/lazy.nvim.git",
+			"--branch=stable", -- latest stable release
+			lazypath,
+		})
 end
+vim.opt.rtp:prepend(lazypath)
 
-local packer_bootstrap = ensure_packer()
+local plugins = {
+	-- INTEGRACION CON GIT
+	"tpope/vim-fugitive",
 
--- Compila la lista de packer cuando se actualiza este archivo
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-  augroup end
-	]])
+	-- REQUISITO DE fugitive PARA :Gbrowse
+	"tpope/vim-rhubarb",
 
-return require("packer").startup(function(use)
-  use 'wbthomason/packer.nvim'
-	use 'tpope/vim-fugitive'                                    -- INTEGRACION CON GIT
-	use 'tpope/vim-rhubarb'                                     -- REQUISITO DE fugitive PARA :Gbrowse
-	use 'lewis6991/gitsigns.nvim'                               -- CONTROL DE CAMBIOS DE UN ARCHIVO
-	use 'windwp/nvim-autopairs'                                 -- CERRADO AUTOMÁTICO DE PARÉNTESIS, LLAVES, COMILLAS, ETC.
-	use 'lukas-reineke/indent-blankline.nvim'                   -- PONE CON ' | ' SEGÚN LOS TABS PARA INDICAR EL CONTENIDO DE UNA ESTRUCTURA
-	use 'norcalli/nvim-colorizer.lua'                           -- COLORIZADO DE CODIGOS HEXADECIMALES y de etiquetas rgb()
-	use 'sheerun/vim-polyglot'                                  -- HIGHLIGHTNING DE LENGUAJES
-	use 'phaazon/hop.nvim'                                      -- MOVERSE MÁS RAPIDO POR NEOVIM CON ',' + 'f'
-	use 'jbyuki/venn.nvim'                                      -- DIBUJAR LINEAS
-	use 'preservim/tagbar'                                      -- ESQUEMA QUE MUESTRA EN UNA LISTA LOS TIPOS DE VARIABLES Y EL NOMBRE DE LAS VARIABLES DECLARADAS.
-	use 'nvim-treesitter/nvim-treesitter'                       -- MEJORA EL DIBUJADO DE LA SINTAXIS
-	use 'nvim-treesitter/nvim-treesitter-context'               -- MUESTRA EN LA PARTE SUPERIOR DENTRO DE LA CLASE, FUNCION, ETC EN LA QUE SE ENCUENTRA
-	use 'nvim-telescope/telescope.nvim' 												-- EXTENSION DE FZF ESCRITO EN LUA
-	use 'nvim-lua/plenary.nvim'																	-- DEPENDENCIA DE TELESCOPE
-	use 'BurntSushi/ripgrep'																		-- DEPENDENCIA DE TELESCOPE
-	use 'mhinz/vim-startify'			                              -- PANTALLA DE INICIO
-	use 'kyazdani42/nvim-web-devicons'                          -- ICONOS PARA VIM-STARTIFY
-	use 'kyazdani42/nvim-tree.lua' 															-- ARBOL DE ARCHIVOS ESCRITO EN LUA (SIMIL A NERDTREE)
-	use 'akinsho/bufferline.nvim' 															-- PESTAÑAS PARA CADA BUFFER (PARTE SUPERIOR)
-	use 'nvim-lualine/lualine.nvim'                            	-- BARRA INFERIOR DE ESTADO CON COLORES
-	use 'kshenoy/vim-signature'                                 -- PONE A LA IZQUIERDA DE LOS NUMEROS DE LINEA LA MARCA(m[letra]) QUE FUE ASIGNADA A LA LINEA
-	use ('folke/tokyonight.nvim', {branch= 'main'})             -- COLORSCHEME DE TOKIO NIGHT
+	-- CONTROL DE CAMBIOS DE UN ARCHIVO
+	"lewis6991/gitsigns.nvim",
 
-	use {
-		'VonHeikemen/lsp-zero.nvim', 															-- INTREGRACION ENTRE NVIM LSP Y LOS SNNIPPETS
-		requires = {
+	-- CERRADO AUTOMÁTICO DE PARÉNTESIS, LLAVES, COMILLAS, ETC.
+	"windwp/nvim-autopairs",
+
+	-- PONE CON ' | ' SEGÚN LOS TABS PARA INDICAR EL CONTENIDO DE UNA ESTRUCTURA
+	"lukas-reineke/indent-blankline.nvim",
+
+	-- COLORIZADO DE CODIGOS HEXADECIMALES y de etiquetas rgb()
+	"norcalli/nvim-colorizer.lua",
+
+
+	-- HIGHLIGHTNING DE LENGUAJES
+	"sheerun/vim-polyglot",
+
+	-- MOVERSE MÁS RAPIDO POR NEOVIM CON ',' + 'f'
+	"phaazon/hop.nvim",
+
+	-- DIBUJAR LINEAS
+	"jbyuki/venn.nvim",
+
+	-- ESQUEMA QUE MUESTRA EN UNA LISTA LOS TIPOS DE VARIABLES Y EL NOMBRE DE LAS VARIABLES DECLARADAS.
+	"preservim/tagbar",
+
+	-- MEJORA EL DIBUJADO DE LA SINTAXIS
+	"nvim-treesitter/nvim-treesitter",
+
+	-- MUESTRA EN LA PARTE SUPERIOR DENTRO DE LA CLASE, FUNCION, ETC EN LA QUE SE ENCUENTRA
+	-- "nvim-treesitter/nvim-treesitter-context",
+
+	-- EXTENSION DE FZF ESCRITO EN LUA
+	"nvim-telescope/telescope.nvim",
+
+	-- DEPENDENCIA DE TELESCOPE
+	"nvim-lua/plenary.nvim",
+
+	-- DEPENDENCIA DE TELESCOPE
+	"BurntSushi/ripgrep",
+
+	-- PANTALLA DE INICIO
+	"mhinz/vim-startify",
+
+	-- ICONOS PARA VIM-STARTIFY
+	"kyazdani42/nvim-web-devicons",
+
+	-- ARBOL DE ARCHIVOS ESCRITO EN LUA (SIMIL A NERDTREE)
+	"kyazdani42/nvim-tree.lua",
+
+	-- PESTAÑAS PARA CADA BUFFER (PARTE SUPERIOR)
+	"akinsho/bufferline.nvim",
+
+	-- BARRA INFERIOR DE ESTADO CON COLORES
+	"nvim-lualine/lualine.nvim",
+
+	-- PONE A LA IZQUIERDA DE LOS NUMEROS DE LINEA LA MARCA(m[letra]) QUE FUE ASIGNADA A LA LINEA
+	"kshenoy/vim-signature",
+
+	-- COLORSCHEME DE TOKIO NIGHT
+	"folke/tokyonight.nvim",
+
+	-- CONFIGURACIONES PARA EL LSP INTEGRADO DE NEOVIM
+	{"neovim/nvim-lspconfig",
+		dependencies = {
 			-- LSP Support
-			{'neovim/nvim-lspconfig'}, 															-- CONFIGURACIONES PARA EL LSP INTEGRADO DE NEOVIM
-			{'williamboman/mason.nvim', 														-- PARA INSTALAR SERVIDORES DE LENGUAJES
-					run = ":MasonUpdate"
-			},
-			{'williamboman/mason-lspconfig.nvim'}, 									-- PARA CONFIGURAR EL LSP (MEJOR INTEGRACIÓN CON LSP)
-			{'WhoIsSethDaniel/mason-tool-installer.nvim'}, 					-- PARA INSTALAR AUTOMATICAMENTE LSPs Y DAPs
+			-- INTREGRACION ENTRE NVIM LSP Y LOS SNNIPPETS
+			"VonHeikemen/lsp-zero.nvim",
+			-- PARA INSTALAR SERVIDORES DE LENGUAJES
+			"williamboman/mason.nvim",
+			-- PARA CONFIGURAR EL LSP (MEJOR INTEGRACIÓN CON LSP)
+			"williamboman/mason-lspconfig.nvim",
+			-- PARA INSTALAR AUTOMATICAMENTE LSPs Y DAPs
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
 			-- Autocompletion
-			{'hrsh7th/nvim-cmp'}, 																	-- SERVIDOR DE AUTOCOMPLETADO
-			{'hrsh7th/cmp-buffer'}, 																-- AUTOCOMPLETADO DE ARCHIVOS
-			{'hrsh7th/cmp-path'}, 																	-- AUTOCOMPLETADO DE RUTAS DEL SISTEMA
-			{'saadparwaiz1/cmp_luasnip'}, 													-- INTEGRACION ENTRE EL SERVIDOR DE AUTOCOMPLETADO Y EL PLUGIN DE SNIPPETS
-			{'hrsh7th/cmp-nvim-lsp'}, 															-- AUTOCOMPLETADO PARA EL LSP DE NVIM (?)
-			{'hrsh7th/cmp-nvim-lua'}, 															-- AUTOCOMPLETADO PARA LA API DE NEOVIM
-			{'ray-x/lsp_signature.nvim'},														-- MUESTRA LOS PARAMETROS MIENTRAS SE ESCRIBE UNA FUNCIÓN
+			-- SERVIDOR DE AUTOCOMPLETADO
+			"hrsh7th/nvim-cmp",
+			-- AUTOCOMPLETADO DE ARCHIVOS
+			"hrsh7th/cmp-buffer",
+			-- AUTOCOMPLETADO DE RUTAS DEL SISTEMA
+			"hrsh7th/cmp-path",
+			-- INTEGRACION ENTRE EL SERVIDOR DE AUTOCOMPLETADO Y EL PLUGIN DE SNIPPETS
+			"saadparwaiz1/cmp_luasnip",
+			-- AUTOCOMPLETADO PARA EL LSP DE NVIM (?)
+			"hrsh7th/cmp-nvim-lsp",
+			-- AUTOCOMPLETADO PARA LA API DE NEOVIM
+			"hrsh7th/cmp-nvim-lua",
+			-- MUESTRA LOS PARAMETROS MIENTRAS SE ESCRIBE UNA FUNCIÓN
+			"ray-x/lsp_signature.nvim",
 			-- Snippets
-			{'L3MON4D3/LuaSnip'}, 																	-- SNIPPETS PARA NEOVIM EN LUA
-			{'rafamadriz/friendly-snippets'}, 											-- COLECCION DE SNIPPETS
+			-- SNIPPETS PARA NEOVIM EN LUA
+			"L3MON4D3/LuaSnip",
+			-- COLECCION DE SNIPPETS
+			"rafamadriz/friendly-snippets",
+		}
+	},
+
+	-- Historial, estilo Git, de cambios
+	"mbbill/undotree",
+
+	-- HISTORIAL DE COPIADO CON TELESCOPE
+	"AckslD/nvim-neoclip.lua",
+	-- CENTRA EL CODIGO AL CENTRO
+	"folke/zen-mode.nvim",
+	-- RESALTA LA PORCION DE CODIGO DEBAJO DEL CURSOR
+	"folke/twilight.nvim",
+	-- PERMITE COMENTAR CON 'gcc'
+	"numToStr/Comment.nvim",
+	-- VIM MULTIPLE CURSORS
+	"mg979/vim-visual-multi",
+	-- TERMINAL DESPLEGABLE
+	"akinsho/toggleterm.nvim",
+	-- INTEGRAR VIM CON TMUX
+	"christoomey/vim-tmux-navigator",
+
+	-- LIVE PREVIEW DE MARKDOWN PARA NEOVIM
+	{"iamcco/markdown-preview.nvim",
+		build = function ()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
+
+
+	-- FILE EXPLORER EN TELESCOPE
+	{"nvim-telescope/telescope-file-browser.nvim",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+			"nvim-lua/plenary.nvim"
+		}
+	},
+
+	-- ASISTENTE PARA LAS TABLAS EN MARKDOWN
+	"dhruvasagar/vim-table-mode",
+
+	-- GENERA LOS INDICES EN MARKDOWN
+	"mzlogin/vim-markdown-toc",
+
+	-- ABRE UN DIRECTIORIO COMO UN BUFFER
+	"stevearc/oil.nvim",
+
+	-- Interfaz del DAP
+	{"rcarriga/nvim-dap-ui",
+		dependencies={
+			-- DAP para vim: cliente para Nvim del debugger
+			"mfussenegger/nvim-dap",
+			-- Muestra junto a la declaración el valor de la variable
+			"theHamsta/nvim-dap-virtual-text",
+			-- Configuración para el DAP de Python
+			"mfussenegger/nvim-dap-python",
+			-- Utiliza Telescope para DAP
+			"nvim-telescope/telescope-dap.nvim"
 		}
 	}
-	-- use 'dstein64/vim-startuptime'  													  -- MUESTRA DE FORMA GRAFICA EL TIEMPO DE ARRANQUE
-	use 'mbbill/undotree'
-	use 'AckslD/nvim-neoclip.lua' 															-- HISTORIAL DE COPIADO CON TELESCOPE
-	use 'folke/zen-mode.nvim' 																	-- CENTRA EL CODIGO AL CENTRO
-	use 'folke/twilight.nvim' 																	-- RESALTA LA PORCION DE CODIGO DEBAJO DEL CURSOR
-	use 'numToStr/Comment.nvim'                                 -- PERMITE COMENTAR CON 'gcc'
-	use 'mg979/vim-visual-multi'        												-- VIM MULTIPLE CURSORS
-	use 'akinsho/toggleterm.nvim' 															-- TERMINAL DESPLEGABLE
-	use 'christoomey/vim-tmux-navigator'  											-- INTEGRAR VIM CON TMUX
-	use({
-			"iamcco/markdown-preview.nvim", 												-- LIVE PREVIEW DE MARKDOWN PARA NEOVIM
-			run = function() vim.fn["mkdp#util#install"]() end,
-	})
-	use {
-			"nvim-telescope/telescope-file-browser.nvim", 						-- FILE EXPLORER EN TELESCOPE
-			requires = { "nvim-telescope/telescope.nvim",
-			"nvim-lua/plenary.nvim" }
-	}
-	use 'dhruvasagar/vim-table-mode' 															-- ASISTENTE PARA LAS TABLAS EN MARKDOWN
-	use 'mzlogin/vim-markdown-toc' 																-- GENERA LOS INDICES EN MARKDOWN
-	use 'stevearc/oil.nvim' 																			-- ABRE UN DIRECTIORIO COMO UN BUFFER
+}
 
-	use({
-		'rcarriga/nvim-dap-ui', 																		-- Interfaz del DAP
-		requires = {'mfussenegger/nvim-dap', 												-- DAP para vim: cliente para Nvim del debugger
-		'theHamsta/nvim-dap-virtual-text', 													-- Muestra junto a la declaración el valor de la variable
-		'mfussenegger/nvim-dap-python', 														-- Configuración para el DAP de Python
-		'nvim-telescope/telescope-dap.nvim'} 												-- Utiliza Telescope para DAP
-	})
-
-	-- Instalación de Plugins en el primer arranque
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
+require("lazy").setup(plugins)
