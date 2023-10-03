4891bed755c251b8dd2c78476a3c52c5bfdd28b8
Ezee1015 <82286822+Ezee1015@users.noreply.github.com>
2023-10-02 22:06:52 -0300

    Cambia el espaciado de un mapping

--- Tema/init/lua/maps.lua
+++ Tema/init/lua/maps.lua
@@ -225,8 +225,8 @@ map('v', '<S-l>'         , '>gv'                             , { }              
 map('v', '<S-h>'         , '<gv'                             , { }                             )
 
 -- File to HTML
-vim.keymap.set('v', '<F11>', '<cmd>TOhtml<CR>', {})
-vim.keymap.set('n', '<F11>', '<cmd>TOhtml<CR>', {})
+map('v', '<F11>'         , '<cmd>TOhtml<CR>'                 , { }                             )
+map('n', '<F11>'         , '<cmd>TOhtml<CR>'                 , { }                             )
 
 -- Hace que pegar sobre una seleccion no reemplaze a lo que se borra
 map('v', '<leader>p'    , '"_dP'                             , { silent= true, noremap= true } )
