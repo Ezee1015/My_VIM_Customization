6b66dc2aed365dd95e68322ae731fbeea65d74a7
Ezee1015 <82286822+Ezee1015@users.noreply.github.com>
2023-10-06 06:56:38 -0300

    Elimina algunas variable que no utilizo más de la configuración básica

--- Tema/init/lua/basic.lua
+++ Tema/init/lua/basic.lua
@@ -4,7 +4,6 @@ vim.o.mouse = "a"
 vim.o.cursorline = true
 vim.cmd "set wildmode=longest:full,full"
 vim.o.ic = true --Ignora la Capitalizacion de las letras en la busqueda = true
-vim.o.updatetime = 300 -- Tiempo de actualizacion de CoC
 vim.o.smartindent = true
 vim.opt.fillchars = {
   vert = "█",
@@ -27,9 +26,6 @@ vim.opt.completeopt= "menu,menuone,noselect"
 vim.o.wildignore=vim.o.wildignore.."*/tmp/*,*.so,*.swp,*.zip,*.pyc,*.db,*.sqlite"
 vim.o.wildignore=vim.o.wildignore.."*.o,*.obj,.git,*.rbc,*.pyc,__pycache__"
 vim.opt.undofile  = true
-vim.opt.undodir = { vim.fn.expand("~/.local/share/nvim/undo") }
--- vim.opt.backupdir = { home .. ".backup//"}
--- vim.opt.directory = { home .. ".swp//"}
 
 vim.cmd ([[
   " Automatically deletes all trailing whitespace and newlines at end of file on save.
@@ -59,10 +55,7 @@ vim.cmd ([[
 -- "" Basic Setup
 -- "*****************************************************************************"
 -- Agrega icono al costado de los folds
-vim.opt.foldcolumn = "1"
-
--- Fix backspace indent
-vim.cmd ([[set backspace=indent,eol,start]])
+-- vim.opt.foldcolumn = "1"
 
 -- Tabs. May be overridden by autocmd rules
 vim.o.tabstop     = 2
@@ -72,9 +65,6 @@ vim.o.expandtab   = true
 
 -- Searching
 vim.o.smartcase   = true
-
--- ¿NECESARIO???
--- vim.o.fileformats={unix,dos,mac}
 
 vim.cmd([[
   if exists('$SHELL')
@@ -87,8 +77,6 @@ vim.cmd([[
 -- "*****************************************************************************
 -- "" Visual Settings
 -- "*****************************************************************************
-vim.o.mousemodel  = "popup"
-
 vim.o.scrolloff   = 3
 
 -- Use modeline overrides
