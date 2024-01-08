edd4dfe6b3dcc5b5e1347726d5533c0901702c81
Ezee1015 <82286822+Ezee1015@users.noreply.github.com>
2024-01-07 21:57:21 -0300

    Add Neorg

---
+++ Tema/init/lua/plugins/neorg.lua
@@ -0,0 +1,82 @@
+local function neorg_config()
+  require("neorg").setup {
+    load = {
+      ["core.defaults"] = {}, -- Loads default behaviour
+      ["core.concealer"] = {}, -- Adds pretty icons to your documents
+      ["core.dirman"] = { -- Manages Neorg workspaces
+      config = {
+        workspaces = {
+          notes = "~/notes",
+        },
+      },
+    },
+    ["core.export"] = {}, -- Export org files
+    ["core.completion"] = {
+      config = {
+        engine = "nvim-cmp"
+      }
+    },
+    ["core.summary"] = {
+      config = {
+        strategy = "by_path", -- TOC by path, and not by the categories
+      },
+    }, -- For a table of contents of multiple files
+    ["core.pivot"] = {}, -- For changing between ordered an unordered lists
+    ["core.presenter"] = {
+      config = {
+        zen_mode = "zen-mode"
+      }
+    }, -- For changing between ordered an unordered lists
+    ["core.itero"] = {}, -- Alt + Enter to new line with line/heading/etc.
+    ["core.integrations.telescope"] = {},
+    -- ["core.ui.calendar"] = {}, -- For changing between ordered an unordered lists
+  },
+}
+end
+
+local function neorg_telescope_config()
+  local neorg_callbacks = require("neorg.core.callbacks")
+
+  neorg_callbacks.on_event(
+    "core.keybinds.events.enable_keybinds",
+    function(_, keybinds)
+      -- Load Telescope from Lazy loading
+      require("lazy").load({
+        wait = true,
+        show = false,
+        plugins = "telescope-dap.nvim",
+        concurrency = 0 -- I don't know what that means
+      })
+
+      -- Map all the below keybinds only when the "norg" mode is active
+      keybinds.map_event_to_mode("norg", {
+        n = { -- Bind keys in normal mode
+          { "<LocalLeader>il", "core.integrations.telescope.insert_link" },
+          { "<LocalLeader>if", "core.integrations.telescope.insert_file_link" },
+
+          { "<LocalLeader>sl", "core.integrations.telescope.find_linkable" },
+          { "<LocalLeader>sf", "core.integrations.telescope.find_norg_files" },
+          { "<LocalLeader>sh", "core.integrations.telescope.search_headings" },
+
+          { "<LocalLeader>sw", "core.integrations.telescope.switch_workspace" },
+        },
+
+        -- i = { -- Bind in insert mode
+        --   { "<C-l>", "core.integrations.telescope.insert_link" },
+        -- },
+      }, { silent = true, noremap = true, })
+    end
+  , _)
+end
+
+return {
+  {"nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    -- tag = "*",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      neorg_config()
+      neorg_telescope_config()
+    end,
+  }
+}
